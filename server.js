40b27c874fe22f992ca9d0a878eae5ab4423b54e
Shreekant Shreeram Yadav <shreekantyadav007@gmail.com>
2024-12-03 16:27:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(cors({ origin: 'https://cloud-db-bk.vercel.app/' }));
 
 const userSchema = new mongoose.Schema(
   {
