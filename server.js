918ee27172ca081d9f3ccd4ea8fe95fd8096f31e
Shreekant <shreey94@gmail.com>
2024-12-03 15:19:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    number: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.get("/", (req, res) => {
+  res.send("Welcome to backend!");
+});
+app.post("/api/users", async (req, res) => {
+  try {
+    const { name, email, number } = req.body;
+    const user = new User({ name, email, number });
+    await user.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to create user" });
+  }
+});
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error fetching users", error: err.message });
+  }
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
